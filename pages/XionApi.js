eac22c092753703e0ed2cf81e5ba9b8d7cec881c
Aeryn Quarmby <67911815+aerynquarmby@users.noreply.github.com>
2023-07-30 22:46:30 +0200

    Update XionApi.js

--- pages/XionApi.js
+++ pages/XionApi.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/Button.module.css";
+import { useAddress } from "@thirdweb-dev/react"; // Import useAddress
 
-const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMHhlQWQzZjNhNzJFMTdiZTEwODQzMzBEOWI3ODBjRTNCZDE1MjhkQUVmIiwiY2xpZW50X2lkIjoiMmJkbDd0MGI4Z3RsNGU0M2ZsZjZucmJyOTgiLCJjbGllbnRfc2VjcmV0IjoiMXIwNWc5Z2VkbjQ2aW9rZGwwdnVyMjE0ODVwNzE4bHVwcXV1NDY4MW5mdW1wYXFxcWtsZyIsImV4cCI6MTY5MzMxNzMxNCwiaWF0IjoxNjkwNzI1MzE0LCJpc3MiOiJYaW9uIEdsb2JhbCBTZXJ2aWNlIEFQSSJ9.yU3xuU5oE2AoLoPhpy9eJjgXzB80YDN7JMcB1DOso98";
-const apiUrl = "https://prodp-api.xion.app/api/v2/single/payment"; // Xion API endpoint
+const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMHhlQWQzZjNhNzJFMTdiZTEwODQzMzBEOWI3ODBjRTNCZDE1MjhkQUVmIiwiY2xpZW50X2lkIjoiNnI2Nmk4Z3ByYmM0czY4cnY1M2doOXNtYjMiLCJjbGllbnRfc2VjcmV0IjoiMXV0N2hwMDlrcGFiajJycXBoZ2lnMTR1Z3U2cTR2ZWMyNGV2czR2OHV1ZGtyOG5yYm1jbCIsImV4cCI6MTY5MjM5MzE2NywiaWF0IjoxNjg5ODAxMTY3LCJpc3MiOiJYaW9uIEdsb2JhbCBTZXJ2aWNlIEFQSSJ9.DLVFK4bspKmsKZkZxP8I82BmnvWl-XgNApJENVDtK2E"; // Use your key
+const apiUrl = "https://prodp-api.xion.app/api/v2/single/payment";
 
-export default function XionApi({ address, amount }) {
+export default function XionApi({ amount }) {
   const [loading, setLoading] = useState(false);
   const [paymentSuccessful, setPaymentSuccessful] = useState(false);
+
+  const address = useAddress(); // Use the hook to get the address
 
   useEffect(() => {
     let timeoutId;
@@ -42,15 +45,11 @@ export default function XionApi({ address, amount }) {
         }
       );
 
-      if (response.data.status === "successful" && response.data.orderCode) {
+      // Check for orderCode and transactionHash to determine success
+      if (response.data.orderCode && response.data.transactionHash) {
         console.log("Payment successful:", response.data);
-        if (response.data.transactionHash) {
-          showSuccessPopup(response.data.transactionHash, response.data.orderCode);
-          setPaymentSuccessful(true);
-        } else {
-          console.warn("Payment successful, but no transaction hash found:", response.data);
-          alert("Payment successful, but no transaction hash found. Please check your wallet or contact support.");
-        }
+        showSuccessPopup(response.data.transactionHash, response.data.orderCode);
+        setPaymentSuccessful(true);
       } else {
         console.error("Payment error:", response.data);
         alert("Payment failed. Please try again.");
@@ -64,38 +63,25 @@ export default function XionApi({ address, amount }) {
   };
 
   const showSuccessPopup = (txHash, orderCode) => {
-    // Create a pop-up element
     const popup = document.createElement("div");
     popup.className = styles.popup;
-  
-    // Create content for the pop-up
     const content = document.createElement("div");
     content.className = styles.popupContent;
-  
     const heading = document.createElement("h2");
     heading.innerText = "Transaction Receipt";
-  
     const orderCodeText = document.createElement("p");
     orderCodeText.innerText = `Order Code: ${orderCode}`;
-  
     const txHashText = document.createElement("p");
-    txHashText.innerText = `Transaction Hash: ${txHash}`;
-  
-    // Append content to the pop-up element
+    txHashText.innerHTML = `Transaction Hash: <a href="https://polygonscan.com/tx/${txHash}" target="_blank" rel="noopener noreferrer">${txHash}</a>`;
     content.appendChild(heading);
     content.appendChild(orderCodeText);
     content.appendChild(txHashText);
     popup.appendChild(content);
-  
-    // Add the pop-up element to the document body
     document.body.appendChild(popup);
-  
-    // Set a timeout to remove the pop-up after 10 seconds
     setTimeout(() => {
       document.body.removeChild(popup);
     }, 10000);
   };
-  
 
   return (
     <div>
